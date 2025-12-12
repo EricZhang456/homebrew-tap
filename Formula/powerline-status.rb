37e6c822e6d399117c37eab2c1d2c117a68ce052
Eric Zhang <ericzhang456@disroot.org>
2025-12-12 17:49:54 +0800

    fix some issues
    Closes #1.
    
    Signed-off-by: Eric Zhang <ericxiang456@gmail.com>

--- Formula/powerline-status.rb
+++ Formula/powerline-status.rb
@@ -1,8 +1,8 @@
 class PowerlineStatus < Formula
   include Language::Python::Virtualenv
 
-  desc "Powerline is a statusline plugin for vim, and provides statuslines and prompts for several other applications"
-  homepage "https://powerline.readthedocs.org/en/latest/"
+  desc "Powerline provides statuslines and prompts for several applications"
+  homepage "https://powerline.readthedocs.io/en/latest/"
   url "https://github.com/powerline/powerline/archive/refs/tags/2.8.4.tar.gz"
   sha256 "9e846af9379b57e410efe264cff3a6b98eb78dd9526e83016776ae5ffc5798f4"
   license "MIT"
@@ -15,8 +15,8 @@ class PowerlineStatus < Formula
 
   # Python 3.13+ fixes
   patch do
-    url "https://github.com/powerline/powerline/pull/2271.patch?full_index=1"
-    sha256 "667ae3e99bb3a6bea4b90b653821827955a8c5248310cb2263bc307fe92cf167"
+    url "https://github.com/powerline/powerline/compare/574bb18...c8640b9.patch?full_index=1"
+    sha256 "f99de5f78c20f992d60db0f72810989dc028083825d5f266c2be75fc69823c7c"
   end
 
   def install
@@ -24,7 +24,8 @@ class PowerlineStatus < Formula
     venv.pip_install resources
     venv.pip_install_and_link buildpath
     (prefix/Language::Python.site_packages(python3)/"homebrew-powerline-status.pth").write venv.site_packages
-    (share/"vim/vimfiles/plugin/powerline.vim").install Dir[venv.site_packages/"powerline/bindings/vim/plugin/powerline.vim"]
+    (share/"vim/vimfiles/plugin/powerline.vim").install Dir[
+      venv.site_packages/"powerline/bindings/vim/plugin/powerline.vim"]
   end
 
   test do
